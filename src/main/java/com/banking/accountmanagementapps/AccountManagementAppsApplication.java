0c329e9c7e7367a53c45ffa49d66aefa5fdec636
arry <arry.pandowo@gmail.com>
2023-06-15 09:56:51 +0700

    all 3 function working properly, creating delete and other exception and field level validation

--- src/main/java/com/banking/accountmanagementapps/AccountManagementAppsApplication.java
+++ src/main/java/com/banking/accountmanagementapps/AccountManagementAppsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication //123
 @ComponentScan
 public class AccountManagementAppsApplication {
 
